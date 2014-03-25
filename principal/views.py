7fe99bc35637ea83a856224fd630ed11f677f2b4
Yohanna Lisnichuk <yolile_3000@hotmail.com>
2014-03-25 15:58:39 -0400

    agregar ingresar e inicio

--- principal/views.py
+++ principal/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.core.mail import EmailMessage
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+def inicio(request):
+    return render_to_response('inicio.html',context_instance=RequestContext(request))
+def ingresar(request):
+  #  if not request.user.is_anonymous():
+   #     return HttpResponseRedirect('/privado')
+    if request.method == 'POST':
+        formulario = AuthenticationForm(request.POST)
+        if formulario.is_valid:
+            usuario = request.POST['username']
+            clave = request.POST['password']
+            acceso = authenticate(username=usuario, password=clave)
+
+            if acceso is not None:
+                if acceso.is_superuser:
+                    login(request, acceso)
+                    return HttpResponseRedirect('/admin')
+                if acceso.is_active:
+                    login(request, acceso)
+                    return HttpResponseRedirect('/privado')
+                else:
+                    return render_to_response('noactivo.html', context_instance=RequestContext(request))
+            else:
+                return render_to_response('nousuario.html', context_instance=RequestContext(request))
+    else:
+        formulario = AuthenticationForm()
+    return render_to_response('ingresar.html',{'formulario':formulario}, context_instance=RequestContext(request))
+
+@login_required(login_url='/ingresar')
+def privado(request):
+    usuario = request.user
+    return render_to_response('privado.html', {'usuario':usuario}, context_instance=RequestContext(request))
