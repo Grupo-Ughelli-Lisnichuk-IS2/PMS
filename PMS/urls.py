7496939c539b32f4f4ceb7d85ba9d46735aa1ec6
Yohanna Lisnichuk <yolile_3000@hotmail.com>
2014-04-05 16:26:03 -0400

    mejoras en las urls

--- PMS/urls.py
+++ PMS/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 from django.contrib import admin
-from principal import views
+from principal import viewsUsuarios
 from django.views.generic import edit, TemplateView
 from django.contrib.auth.decorators import login_required, permission_required
 admin.autodiscover()
@@ -10,18 +10,17 @@ urlpatterns = patterns('',
      url(r'^$',TemplateView.as_view(template_name='inicio.html')),
     # url(r'^blog/', include('blog.urls')),
     #url(r'^admin/', include(admin.site.urls)),
-    url(r'^usuarios/$',login_required(views.lista_usuarios, '/usuarios', '/login')),
-url(r'^usuarios/(?P<id_user>\d+)$','principal.views.detalle_usuario'),
-url(r'^modificar/(?P<id_user>\d+)$','principal.views.modificar_usuario'),
-    url(r'^search/$',views.search, name='buscar_usuarios'),
+    url(r'^usuarios/$',login_required(viewsUsuarios.lista_usuarios, '/usuarios', '/login')),
+url(r'^usuarios/(?P<id_user>\d+)$', 'principal.viewsUsuarios.detalle_usuario'),
+url(r'^modificar/(?P<id_user>\d+)$', 'principal.viewsUsuarios.modificar_usuario'),
+    url(r'^search/$',viewsUsuarios.buscarUsuario, name='buscar_usuarios'),
     url(r'^principal/$',login_required(TemplateView.as_view(template_name='principal.html'), '/', '/login')),
-    url(r'^login/$', views.LoginView.as_view(), name='login'),
-    url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
+    url(r'^login/$', viewsUsuarios.LoginView.as_view(), name='login'),
+    url(r'^logout/$', viewsUsuarios.LogoutView.as_view(), name='logout'),
     # register
-    url(r'^register/$', login_required(views.RegisterView.as_view(), '/register', '/login')),
-    url(r'^modificar/$', views.UsuarioChangeStateView.as_view(), name='modificar'),
+    url(r'^register/$', login_required(viewsUsuarios.RegisterView.as_view(), '/register', '/login')),
     url(r'^register/success/$',
-        views.RegisterSuccessView.as_view(
+        viewsUsuarios.RegisterSuccessView.as_view(
         ), name='register-success'),
 
         )
