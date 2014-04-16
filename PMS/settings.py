3cafd7004f98f841e4c04fd44c8a3bb5730eb985
Yohanna Lisnichuk <yolile_3000@hotmail.com>
2014-04-16 16:45:32 -0400

    model proyecto

--- PMS/settings.py
+++ PMS/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'usuarios',
     'fases',
+    'proyectos',
 )
 
 
---
+++ proyectos/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+ESTADOS = (
+
+    ('PEN', 'Pendiente'),
+    ('ANU','Anulado'),
+    ('ACT', 'Activo'),
+    ('FIN','Finalizado')
+)
+
+
+class proyecto(models.Model):
+    nombre= models.CharField(max_length=100, verbose_name='Nombre',unique=True)
+    descripcion= models.TextField(verbose_name='Descripcion')
+    fecha_ini=models.DateField(null=False)
+    fecha_fin=models.DateField(null=False)
+    estado=models.CharField(max_length=3,choices= ESTADOS, default='PEN')
+    lider = models.ForeignKey(User)
+    observaciones = models.TextField(verbose_name='Observaciones')
