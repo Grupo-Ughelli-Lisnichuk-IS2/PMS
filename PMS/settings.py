56206a68c082217ad29ff2619181225b80d76876
Yohanna Lisnichuk <yolile_3000@hotmail.com>
2014-04-19 19:46:50 -0400

    arreglo en settings

--- PMS/settings.py
+++ PMS/settings.py
@@ -50,7 +50,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-    'test_utils',
     'usuarios',
     'fases',
     'proyectos',
