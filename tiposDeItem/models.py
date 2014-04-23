bbc1f6903f1e7a6c4769b6f68418d822f6ee9b58
viktor992 <viktor992@hotmail.com>
2014-04-23 19:58:51 -0400

    verbose_name agregado a campos de TiposDeItem/models

--- tiposDeItem/models.py
+++ tiposDeItem/models.py
@@ -10,15 +10,15 @@ TIPOS = (
 )
 
 class TipoItem(models.Model):
-    nombre=models.CharField(max_length=100, null=False)
-    descripcion=models.TextField(max_length=140, null=True)
-    fase=models.ForeignKey(Fase, related_name='fase', null=True)
+    nombre=models.CharField(max_length=100, null=False, verbose_name="Nombre")
+    descripcion=models.TextField(max_length=140, null=True, verbose_name="Descripcion")
+    fase=models.ForeignKey(Fase, related_name='fase', null=True, verbose_name="Fase")
 
 class Atributo(models.Model):
-    nombre=models.CharField(max_length=100, null=Fase)
-    tipo=models.CharField(max_length=3,choices=TIPOS, default='TEX' )
-    valorDefecto=models.CharField(max_length=140)
-    tipoItem=models.ForeignKey(TipoItem, related_name='tipoItem')
+    nombre=models.CharField(max_length=100, null=Fase, verbose_name="Nombre")
+    tipo=models.CharField(max_length=3,choices=TIPOS, default='TEX', verbose_name="Tipo de Dato" )
+    valorDefecto=models.CharField(max_length=140, verbose_name="Valor por defecto")
+    tipoItem=models.ForeignKey(TipoItem, related_name='tipoItem', verbose_name="Tipo de Item")
 
 #class AtributoItem(models.Model):
 #    id_item=models.SmallIntegerField(null=False);
