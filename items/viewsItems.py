e9228dfaca778b8a48500b989c82185cfb5bfdd2
Yohanna Lisnichuk <yolile_3000@hotmail.com>
2014-04-24 10:10:41 -0400

    agregar Abrir Fase (modulo de desarrollo)

--- items/viewsItems.py
+++ items/viewsItems.py
@@ -15,14 +15,17 @@ def listar_proyectos(request):
     vista para listar los proyectos asignados a un usuario expecifico
     '''
     usuario = request.user
+    #proyectos del cual es lider y su estado es activo
     proyectosLider = Proyecto.objects.filter(lider_id=usuario.id, estado='ACT')
 
-    roles=Group.objects.filter(user__id=usuario.id)
+    roles=Group.objects.filter(user__id=usuario.id).exclude(name='Lider')
     fases=[]
     proyectos=[]
+    #las fases de en las cuales el usuario tiene un rol
     for rol in roles:
         fase=Fase.objects.get(roles=rol.id)
         fases.append(fase)
+    #los proyectos a los que pertenecen esas fases
     for fase in fases:
         proyecto=Proyecto.objects.get(id=fase.proyecto_id)
         if proyecto.estado=='ACT':
@@ -30,3 +33,25 @@ def listar_proyectos(request):
     for p in proyectosLider:
         proyectos.append(p)
     return render_to_response('items/abrir_proyecto.html', {'datos': proyectos}, context_instance=RequestContext(request))
+
+def listar_fases(request, id_proyecto):
+
+    '''
+    vista para listar las fases asignadas a un usuario de un proyecto especifico
+    '''
+    fasesProyecto=Fase.objects.filter(proyecto_id=id_proyecto)
+    usuario = request.user
+    proyecto=Proyecto.objects.get(id=id_proyecto)
+    fases=[]
+    if usuario.id==proyecto.lider_id:
+        fases=fasesProyecto
+    else:
+        roles=Group.objects.filter(user__id=usuario.id).exclude(name='Lider')
+        for rol in roles:
+            for f in fasesProyecto:
+                ff=Fase.objects.filter(id=f.id,roles__id=rol.id)
+                for fff in ff:
+                    fases.append(fff)
+
+    return render_to_response('items/abrir_fase.html', {'datos': fases}, context_instance=RequestContext(request))
+
