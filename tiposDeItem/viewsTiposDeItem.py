f8a3e4578da6e5b8696c5e7fad249ac8277951cb
Yohanna Lisnichuk <yolile_3000@hotmail.com>
2014-04-28 21:36:36 -0400

    agregar validaciones en tipos de atributos

--- tiposDeItem/viewsTiposDeItem.py
+++ tiposDeItem/viewsTiposDeItem.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.contrib.auth.decorators import login_required, permission_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, render_to_response, get_object_or_404
@@ -5,6 +6,7 @@ from django.contrib import messages
 
 # Create your views here.
 from django.template import RequestContext
+import time
 from PMS import settings
 from fases.models import Fase
 from tiposDeItem.formsTiposDeItem import TipoItemForm, AtributoForm, TipoItemModForm
@@ -55,6 +57,32 @@ def detalle_tipoItem(request, id_tipoItem):
     return render_to_response('tiposDeItem/detalle_tipoDeItem.html', {'datos': dato, 'atributos': atributos}, context_instance=RequestContext(request))
 
 
+def validarAtributo(request, datatype, valor):
+    '''
+    Funcion para validar que el valor por defecto ingresado para un atributo concuerde con el tipo especificado
+    '''
+    if datatype=="FEC":
+            try:
+                fecha=datetime.strptime(str(valor),'%d/%m/%Y')
+            except ValueError:
+                    messages.add_message(request, settings.DELETE_MESSAGE, "Fecha incorrecta debe ser del tipo DD/MM/AAAA")
+                    return False
+    else:
+                if datatype=="NUM":
+                    a= valor.isdigit()
+                    if not a:
+                        messages.add_message(request, settings.DELETE_MESSAGE, "Valor no es numerico")
+                        return False
+
+                else:
+                    if datatype=="LOG":
+                        if valor!= "Verdadero" and valor!="Falso":
+                            messages.add_message(request,settings.DELETE_MESSAGE, "Valor no es booleano. Debe ser Verdadero o Falso")
+                            return False
+
+    return True
+
+
 def crear_atributo(request, id_tipoItem):
     '''
     vista para crear un tipo de atributo, que consta de un nombre, un tipo, un valor por defecto
@@ -67,13 +95,17 @@ def crear_atributo(request, id_tipoItem):
 
         if atributo_form.is_valid():
             tipoItem = TipoItem.objects.filter(id=id_tipoItem)#uso filter y no get porque atributo.tipoItem=tipoItem requiere que tipoItem sea Iterable
-            atributo= Atributo(nombre = request.POST["nombre"], tipo = request.POST["tipo"], valorDefecto = request.POST["valorDefecto"])
-            atributo.save()
-            atributo.tipoItem=tipoItem
-            atributo.save()
-            tipoItem2 = TipoItem.objects.get(id=id_tipoItem)
-            id_fase = tipoItem2.fase_id
-            return HttpResponseRedirect('/tiposDeItem/modificar/'+str(id_tipoItem))
+
+            valor=request.POST["valorDefecto"]
+            datatype=request.POST["tipo"]
+            if validarAtributo(request,datatype,valor):
+                atributo= Atributo(nombre = request.POST["nombre"], tipo = request.POST["tipo"], valorDefecto = request.POST["valorDefecto"])
+                atributo.save()
+                atributo.tipoItem=tipoItem
+                atributo.save()
+                tipoItem2 = TipoItem.objects.get(id=id_tipoItem)
+                id_fase = tipoItem2.fase_id
+                return HttpResponseRedirect('/tiposDeItem/modificar/'+str(id_tipoItem))
     else:
         # formulario inicial
         atributo_form = AtributoForm()
