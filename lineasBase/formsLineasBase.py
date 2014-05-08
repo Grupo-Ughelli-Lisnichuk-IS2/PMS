2a78e7ec434b43b6c7de872cb752b8e34db7887b
Yohanna Lisnichuk <yolile_3000@hotmail.com>
2014-05-08 01:29:46 -0400

    crear linea base completo

--- lineasBase/formsLineasBase.py
+++ lineasBase/formsLineasBase.py
@@ -15,7 +15,7 @@ class LineaBaseForm(forms.ModelForm):
     items = forms.ModelMultipleChoiceField(queryset=Item.objects.none(), widget=forms.CheckboxSelectMultiple(), required=True)
     def __init__(self, fase, *args, **kwargs):
         super(LineaBaseForm, self).__init__(*args, **kwargs)
-        self.fields['items'].queryset = Item.objects.filter(estado='VAL', tipo_item=fase, lineaBase=None)
+        self.fields['items'].queryset = Item.objects.filter(estado='VAL', tipo_item__fase=fase.id, lineaBase=None)
 
     class Meta:
             model= LineaBase
